1df40e217542b627e78507aa2cdf4cb6ee66a27d
lucky-hee <gobaksa4@naver.com>
2018-05-28 15:38:40 +0900

    make flask project

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return 'Index Page'
+    return "hi! flask!"
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        c()
+    else:
+        show_the_login_form()
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+#url_for('static', filename='style.css')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
