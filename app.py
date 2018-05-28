ee9c44e82682f871c5c6c84c3fa0b7fb36695fc8
lucky-hee <gobaksa4@naver.com>
2018-05-28 18:51:11 +0900

    Add Bootstrap Theme

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lucy:abcd123@localhost/flasksecond'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#FSADeprecationWarning: SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and will be disabled by default in the future. Set it to True or False to suppress this warning. 'SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead and '
+app.debug = True
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @app.route('/')
 def index():
-    return "hi! flask!"
+    return render_template('index.html')
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        c()
-    else:
-        show_the_login_form()
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-
-#url_for('static', filename='style.css')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run()
